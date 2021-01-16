6b7372f774812b5ae853808ac269572d651a7f57
vvEii <coopershang125@gmail.com>
2021-01-15 20:43:34 -0800

    Fetch movies

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
+
+const showResult = (title: string): void => {
+  console.log(title);
+  axios
+    .get(`http://www.omdbapi.com/?s=${title}&apikey=a9adfcbd`)
+    .then((res) => console.log(res.data.Search))
+    .catch((err) => console.log(err));
+};
 
 export const SearchBar = (): React.ReactElement => {
-  return <TextField id="standard-basic" label="Search Movies" onChange={(event) => console.log(event.target.value)} />;
+  return <TextField id="standard-basic" label="Search Movies" onChange={(event) => showResult(event.target.value)} />;
 };
