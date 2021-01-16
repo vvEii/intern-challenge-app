d149fa6472b7bb7fa8c5206469a3bd9a91113daf
vvEii <coopershang125@gmail.com>
2021-01-15 22:20:51 -0800

    Fetch movies using useEffect

--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 import { MoviesList } from './MoviesList';
 import { SearchBar } from './SearchBar';
 import { NominationsList } from './NominationsList';
@@ -6,6 +7,13 @@ import './MovieSearch.css';
 
 export const MovieSearch = (): React.ReactElement => {
   const [term, setTerm] = useState('');
+  useEffect(() => {
+    axios
+      .get(`http://www.omdbapi.com/?s=${term}&apikey=a9adfcbd&page=1&type=movie`)
+      .then((res) => console.log(res.data.Search))
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
     <Fragment>
       <h2>The Shoppies</h2>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-//import axios from 'axios';
 
 interface IProps {
   term: string;
   onChange: (term: string) => void;
 }
-
-// const showResult = (term: string): void => {
-//   console.log(term);
-//   axios
-//     .get(`http://www.omdbapi.com/?s=${term}&apikey=a9adfcbd&type=movie`)
-//     .then((res) => console.log(res.data.Search))
-//     .catch((err) => console.log(err));
-// };
 
 export const SearchBar = (props: IProps): React.ReactElement => {
   return (
