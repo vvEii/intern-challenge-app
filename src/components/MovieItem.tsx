e2cff74c70ced634f4dcc918a9700d48f4a3d069
vvEii <coopershang125@gmail.com>
2021-01-15 20:30:51 -0800

    Add temp MovieItem component

---
+++ src/components/MovieItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MovieItem = (): React.ReactElement => {
+  return (
+    <li>
+      <p>movies name</p>
+      <button>Nominate</button>
+    </li>
+  );
+};
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 export const SearchBar = (): React.ReactElement => {
-  return (
-    <TextField id="standard-basic" label="Search Exercises" onChange={(event) => console.log(event.target.value)} />
-  );
+  return <TextField id="standard-basic" label="Search Movies" onChange={(event) => console.log(event.target.value)} />;
 };
